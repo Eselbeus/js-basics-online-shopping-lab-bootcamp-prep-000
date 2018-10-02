2eb9f662801185f559849c4ff5543d3802522bed
eselbeus <eselbeus@gmail.com>
2018-10-02 09:22:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,5 +67,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  
+  if (cardNumber === undefined){
+    "Sorry, we don't have a credit card on file for you."
+  }
 }

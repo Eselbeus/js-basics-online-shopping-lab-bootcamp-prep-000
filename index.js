99b4b0d6818a760ec8fc2aef75ce77c1701b20ee
eselbeus <eselbeus@gmail.com>
2018-10-02 08:56:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ function viewCart() {
   for (let i = 0; i < cart.length; i++){
     let current = cart[i];
     contents += `${current.itemName} at $${current.itemPrice}`;
-    if (i === cart.length -1){
+    if (i === cart.length -2){
       contents += ", and ";
     }
-    else if (i === cart.length){
+    else if (i === cart.length -1){
       content += ".";
     }
     else{

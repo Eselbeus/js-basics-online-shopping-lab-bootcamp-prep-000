5ef5b76903408511d08bb99c1db7844bc619e60a
eselbeus <eselbeus@gmail.com>
2018-10-02 09:08:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,7 +53,12 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  if 
+  for (let i = 0; i < cart.length; i++){
+    let current = cart[i];
+    if (cart.itemName === item){
+      cart.spice()
+    }
+  }
 }
 
 function placeOrder(cardNumber) {

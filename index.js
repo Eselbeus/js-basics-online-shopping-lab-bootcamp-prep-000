afa764deac55a08066740df49a6d59d4f8843dec
eselbeus <eselbeus@gmail.com>
2018-10-02 08:44:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var newItem = {
+   itemName: item,
+   itemPrice: Math.floor(Math.random() * Math.floor(100)) +1
+ }
+ cart.push(newItem);
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
